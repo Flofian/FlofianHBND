11d801b18d6b6b15ed35f33695f0fb0d958e9e53
Flofian <flofian.neu@gmail.com>
2023-09-14 21:26:41 +0200

    less indents, check ready

--- main.lua
+++ main.lua
@@ -7,30 +7,33 @@ local common = module.load(header.id, 'common')
 local circle_quality = 32
 
 local function amplifyAutoattack(spell)
-    if spell.isBasicAttack then
-        if common.isAlly(spell.owner) and common.isEnemy(spell.target) then
-            print('Found Basic Attack: ' .. spell.name)
-            print('Target: ' .. spell.target.charName)
-            print('Owner: ' .. spell.owner.charName)
-            if spell.owner == player or spell.owner.vec3:dist(player.vec3)<menu.passive.passiveRange:get() then
-                local directenemies = 0
-                for i=0, objManager.enemies_n-1 do
-                    local enemy = objManager.enemies[i]
-                    if enemy.isVisible and enemy.isTargetable and not enemy.isDead and player.pos:dist(spell.target.pos)<menu.q.qRange:get() then
-                        directenemies = directenemies + 1
-                    end
+    if not menu.automatic.autoQamplify:get() then return end
+    if not spell.isBasicAttack then return end
+    if player:spellSlot(0).state ~= 0 then return end
+    if common.isAlly(spell.owner) and common.isEnemy(spell.target) then
+        print('Found Basic Attack: ' .. spell.name)
+        print('Target: ' .. spell.target.charName)
+        print('Owner: ' .. spell.owner.charName)
+        if spell.owner == player or spell.owner.vec3:dist(player.vec3)<menu.passive.passiveRange:get() then
+            local directenemies = 0
+            for i=0, objManager.enemies_n-1 do
+                local enemy = objManager.enemies[i]
+                if enemy.isVisible and enemy.isTargetable and not enemy.isDead and player.pos:dist(enemy.pos)<menu.q.qRange:get() then
+                    directenemies = directenemies + 1
                 end
-                if directenemies >= menu.automatic.autoQamplifydirect:get() then
-                    if 100*player.mana/player.maxMana > menu.automatic.autoQmana:get() then
-                        print('Amplifying Auto Attack')
-                        player:castSpell('self', 0)
-                    end
-                end
-                
             end
+            print('Direct Enemies: ' .. directenemies)
+            if directenemies >= menu.automatic.autoQamplifydirect:get() then
+                if 100*player.mana/player.maxMana > menu.automatic.autoQmana:get() then
+                    print('Amplifying Auto Attack')
+                    player:castSpell('self', 0)
+                end
+            end
+            
         end
     end
 end
+
 cb.add(cb.cast_finish, amplifyAutoattack)
 
 local function drawRanges()
